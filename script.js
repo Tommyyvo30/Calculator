c61a8243ee77c3ff854ca0a8624147dd08819216
Tommyyvo30 <tommyyvo.tv@gmail.com>
2023-03-28 22:40:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,149 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    // Display error message if user tries to divide by zero
+    return "Error: Cannot divide by zero";
+  }
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return NaN;
+  }
+}
+
+const display = document.querySelector(".screen-text");
+let firstNum = null;
+let operator = null;
+let secondNum = null;
+let isNewOperand = true;
+let previousValue = null;
+
+function clear() {
+  display.textContent = "0";
+  firstNum = null;
+  operator = null;
+  secondNum = null;
+  isNewOperand = true;
+}
+
+function updateDisplay(value) {
+  if (display.textContent === "0" || isNewOperand) {
+    display.textContent = value;
+    isNewOperand = false;
+  } else {
+    display.textContent += value;
+  }
+}
+
+function handleNumberClick(event) {
+  const number = event.target.dataset.number;
+  updateDisplay(number);
+}
+
+function handleOperatorClick(event) {
+  const newOperator = event.target.dataset.operator;
+  if (operator !== null && secondNum !== null) {
+    const result = operate(operator, firstNum, secondNum);
+    previousValue = `${firstNum} ${operator} ${secondNum}`;
+    document.querySelector(".previous-value").textContent = previousValue;
+    display.textContent = result;
+    firstNum = result;
+    operator = newOperator;
+    secondNum = null;
+  } else {
+    firstNum = parseFloat(display.textContent);
+    operator = newOperator;
+    if (previousValue === null) {
+      previousValue = `${firstNum} ${operator}`;
+    } else {
+      previousValue = `${previousValue} ${secondNum} ${operator}`;
+    }
+    document.querySelector(".previous-value").textContent = previousValue;
+  }
+  isNewOperand = true;
+}
+
+function handleEqualsClick() {
+  if (operator !== null && secondNum === null) {
+    secondNum = parseFloat(display.textContent);
+    const result = operate(operator, firstNum, secondNum);
+    previousValue = `${firstNum} ${operator} ${secondNum}`;
+    document.querySelector(".previous-value").textContent = previousValue;
+    display.textContent = result;
+    firstNum = result;
+    operator = null;
+    secondNum = null;
+  }
+  isNewOperand = true;
+}
+
+const numberButtons = document.querySelectorAll(".calc-btn[data-number]");
+numberButtons.forEach((button) => {
+  button.addEventListener("click", handleNumberClick);
+});
+
+const operatorButtons = document.querySelectorAll(
+  ".operator-btn[data-operator]"
+);
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", handleOperatorClick);
+});
+
+const equalsButton = document.querySelector(".equals-btn");
+equalsButton.addEventListener("click", handleEqualsClick);
+
+const clearButton = document.querySelector(".clear-btn");
+clearButton.addEventListener("click", clear);
+
+// Added event listeners for decimal and backspace buttons
+const decimalButton = document.querySelector(".decimal-btn");
+decimalButton.addEventListener("click", handleDecimalClick);
+
+const backspaceButton = document.querySelector(".backspace-btn");
+backspaceButton.addEventListener("click", handleBackspaceClick);
+
+function handleDecimalClick() {
+  if (!display.textContent.includes(".")) {
+    display.textContent += ".";
+  }
+}
+
+function handleBackspaceClick() {
+  if (display.textContent.length === 1) {
+    display.textContent = "0";
+  } else {
+    display.textContent = display.textContent.slice(0, -1);
+  }
+}
+
+function handleChangeSignClick() {
+  const currentValue = parseFloat(display.textContent);
+  if (!isNaN(currentValue)) {
+    display.textContent = (-1 * currentValue).toString();
+  }
+}
+
+const changeSignButton = document.querySelector(".change-sign-btn");
+changeSignButton.addEventListener("click", handleChangeSignClick);
